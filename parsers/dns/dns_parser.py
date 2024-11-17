eec76708dd9f2fb5b9c9f1613b6a8b73a074b3d0
sasha <sasha.ignatov93@gmail.com>
2024-11-18 00:09:36 +0500

    dns parser на request'ах (без цены)

---
+++ parsers/dns/dns_parser.py
@@ -0,0 +1,30 @@
+from config import headers, cookies
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_product(product='', price=''):
+    # url = 'https://www.dns-shop.ru/search/'
+    params = {
+        'q': 'realme 8',
+        'category': '17a8a01d16404e77',
+    }
+
+    response = requests.get('https://www.dns-shop.ru/search/', params=params, cookies=cookies, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    product_div_tag = soup.find('div', attrs={'data-id': 'product'})
+
+    href = product_div_tag.find('a', recursive=False).get('href')
+    link = 'https://www.dns-shop.ru' + href
+    product_title = product_div_tag.find('a', recursive=False).find('span').text
+
+    feedback = product_div_tag.find('div',
+                                    attrs={'class': 'catalog-product__stat'},
+                                    recursive=False).find('a', recursive=False)
+    feedback_score = feedback.get('data-rating')
+    feedback_count = feedback.text
+
+    return link, product_title, feedback_score, feedback_count
+
+print(get_product())
