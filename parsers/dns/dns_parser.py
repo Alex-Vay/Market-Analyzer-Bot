b51e8001716630b790c76867162dc5699e913406
sasha <sasha.ignatov93@gmail.com>
2024-12-28 02:27:58 +0500

    fix parser(на самом сайте изменили пару атрибутов)

--- parsers/dns/dns_parser.py
+++ parsers/dns/dns_parser.py
@@ -2,6 +2,7 @@ import re
 
 from bs4 import BeautifulSoup
 import undetected_chromedriver as uc
+from selenium.common import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def get_product_price(soup):
     product_price = soup.find('div', attrs={'class': 'product-buy'}).find('div', attrs={
-        'class': 'product-buy__price-wrap'}).find('div', attrs={'class': 'product-buy__price'}).text
+        'class': 'product-buy__price-wrap'}).find('div', attrs={'class': 'product-buy__price'}).contents[0].strip()
     return product_price
 
 
@@ -25,7 +26,7 @@ def get_product_rating_and_feedback(soup):
 def get_product_title_and_url(soup):
     product_title_and_url = soup.find('a', attrs={'class': 'catalog-product__name'})
     product_url = 'https://www.dns-shop.ru' + product_title_and_url.get('href')
-    product_title = product_title_and_url.find('span').text
+    product_title = product_title_and_url.find('span').text[:75] + '...'
     return product_title, product_url
 
 
@@ -42,22 +43,31 @@ def get_product(item_name=""):
 
     options.add_argument(f'user-agent={user_agent}')
     driver = uc.Chrome(options=options)
-    driver.get(f'https://www.dns-shop.ru/search/?q={item_name}')
 
-    css_selector = "#search-results .products-list__content .catalog-product"
-    # прокрутка до элемента, т.к. он может быть ниже
-    # element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
-    driver.execute_script("window.scrollTo(0, 1500)")
-    # подождем пока дозагрузится цена
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, css_selector + ' .product-buy .product-buy__price')))
-    product_div = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
-    product_div_source = product_div.get_attribute('outerHTML')
-    driver.quit()
-    soup = BeautifulSoup(product_div_source, 'lxml')
-    extracted_data = extract_data(soup)
-    return extracted_data
+    driver.get(f'https://www.dns-shop.ru/search/?q={item_name}')
+    try:
+
+        css_selector = "#search-results .products-list__content .catalog-product"
+        # прокрутка до элемента, т.к. он может быть ниже
+        # element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
+        # driver.execute_script("window.scrollTo(0, 1500)")
+        # подождем пока дозагрузится цена
+        WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, css_selector + ' .product-buy')))
+        driver.execute_script("window.stop();")
+        element = driver.find_element(By.CSS_SELECTOR, css_selector)
+        driver.execute_script("arguments[0].scrollIntoView();", element)
+
+        product_div = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector)))
+        product_div_source = product_div.get_attribute('outerHTML')
+        soup = BeautifulSoup(product_div_source, 'lxml')
+        extracted_data = extract_data(soup)
+        return extracted_data
+    except TimeoutException:
+        return 'Ничего не найдено'
+    finally:
+        driver.quit()
 
 
 if __name__ == '__main__':
-    print(get_product('honor magicbook 15'))
+    print(get_product('realme 12'))
--- parsers/ozon/ozon_parser.py
+++ parsers/ozon/ozon_parser.py
@@ -1,12 +1,22 @@
 import difflib
 import re
+import time
 
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import undetected_chromedriver as uc
+
+
+def get_rating(rating_object):
+    if rating_object:
+        rating_feedback = rating_object.find_all('span', limit=2)
+        product_rating_str = f"Рейтинг: {rating_feedback[0].text}, {rating_feedback[1].text}"
+        return product_rating_str
+    return "Рейтинг: отсутствует"
 
 
 def get_product(item_name='телефон realme 10 черный'):
@@ -14,28 +24,31 @@ def get_product(item_name='телефон realme 10 черный'):
     item_name: товар, который вводит пользователь в боте'''
 
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'  # chrome
-    options = webdriver.ChromeOptions()
+    options = uc.ChromeOptions()
     # options.add_argument('--headless')
     options.add_argument(f'user-agent={user_agent}')
-    driver = webdriver.Chrome(options=options)
-    url = f'https://www.ozon.ru/search/?text={item_name}&from_global=true' #&sorting=price
+    driver = uc.Chrome(options=options)
+    url = f'https://www.ozon.ru/search/?text={item_name}&from_global=true'  # &sorting=price
     driver.get(url)
-    # pass captcha
-    button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'reload-button')))
-    button.click()
     driver.implicitly_wait(3)
+
     # Ожидание загрузки страницы
     WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '#paginatorContent .tile-root .tile-hover-target')))
+        EC.presence_of_element_located((By.CSS_SELECTOR, '#paginatorContent .tile-root')))
+
     # получаем карточки
     products = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH,
                                                                                     "//div[@id='paginatorContent']//div[@data-index < 1]")))
-    #  значение атрибута data-index (порядковый номер карточки) как id
+
+    element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.container div[data-widget="searchResultsSort"] input')))
+    driver.execute_script("return arguments[0].scrollIntoView(true);", element)
+    # #  значение атрибута data-index (порядковый номер карточки) как id
     titles_and_urls = {
         int(product.get_attribute('data-index')): product.find_element(
-            By.CSS_SELECTOR, '.tile-hover-target .tsBody500Medium').text for product in products}
+            By.CSS_SELECTOR, '.tile-clickable-element .tsBody500Medium').text for product in products}
 
-    best_match_index = None
+    best_match_index = 0
     best_ratio = 0
 
     for index, product_title in titles_and_urls.items():
@@ -52,19 +65,16 @@ def get_product(item_name='телефон realme 10 черный'):
             continue
     product_card = products[best_match_index]
     soup = BeautifulSoup(product_card.get_attribute('outerHTML'), 'lxml')
-    product_url = "https://www.ozon.ru" + soup.find('a', attrs={'class': 'tile-hover-target'}).get('href')
+    product_url = "https://www.ozon.ru" + soup.find('a', attrs={'class': 'tile-clickable-element'}).get('href')
     product_title = titles_and_urls[best_match_index]
     product_price = soup.find('span', attrs={'class': 'tsHeadline500Medium'}).text
-    try:
-        rating_feedback = soup.find('div', attrs={'class': 'tsBodyMBold'}).find_all('span', attrs={'q1'})
-        product_rating_feedback = f"Рейтинг: {rating_feedback[0].text}, {rating_feedback[1].text}"
-    except:
-        product_rating_feedback = f"Рейтинг: отсутствует"
+    rating_data = soup.find('div', attrs={'class': 'tsBodyMBold'})
+    product_rating_feedback = get_rating(rating_data)
     driver.quit()
-    return product_title, re.sub("[^0-9]", "", product_price), product_rating_feedback, product_url
+    return product_title, re.sub("[^0-9]", "", product_price) + '₽', product_rating_feedback, product_url
 
 
 if __name__ == '__main__':
     print('Поиск товара...')
-    print(get_product("ноутбук Lenovo"))
+    print(get_product("realme 12"))
     print('Поиск завершен.')
